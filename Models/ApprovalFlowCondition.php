db423d9e01443d80c22fd7d94b29d59b05c2c38d
Aditya Wiguna <adityawiguna0@gmail.com>
2024-12-14 17:43:23 +0800

    (adding) add new model and approval condition

---
+++ Models/ApprovalFlowCondition.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ibinet\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
+
+class ApprovalFlowCondition extends Model
+{
+    use HasFactory;
+    protected $table = 'approval_flow_conditions';
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'created_at', 'updated_at'
+    ];
+
+    /**
+     *  Setup model event hooks
+     */
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->id = (string) Uuid::uuid4();
+        });
+    }
+}
