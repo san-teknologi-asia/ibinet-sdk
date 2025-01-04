e8cf566f8b552196767c0cb64b19b00a204e5c41
jungrama <work@jungrama.com>
2025-01-04 10:40:23 +0800

    chore: remove unused expense report location relation

--- Models/ExpenseReportLocation.php
+++ Models/ExpenseReportLocation.php
@@ -27,33 +27,16 @@ class ExpenseReportLocation extends Model
 
     public function getInfoAttribute()
     {
-        $data = Remote::find($this->remote_id);
-
-        if($this->remote_id != null){
-            $text = "<b>" . $data->name . "</b><br/>" .($data->remoteType->name ?? '-')."<br/>";
-            $text .= "<b>IP LAN : </b>" . $data->ip_lan . "<br/>";
-            $text .= "<b>IP P2P Modem : </b>" . $data->ip_p2p_modem . "<br/>";
-            $text .= "<b>Site ID : </b>" . ($data->site_id ? $data->site_id : "-");
-        } else {
-            $text = $this->work_location;
-        }
+        $text .= "<b>Region : </b>" . $this->region->name ?? "-" . "<br/>";
+        $text .= "<b>Project : </b>" . $this->project->name ?? "-";
 
         return $text;
     }
 
-    public function client()
-    {
-        return $this->belongsTo('Ibinet\Models\Client')->withTrashed();
-    }
 
-    public function district()
+    public function region()
     {
-        return $this->belongsTo('Ibinet\Models\District');
-    }
-
-    public function remote()
-    {
-        return $this->belongsTo('Ibinet\Models\Remote');
+        return $this->belongsTo('Ibinet\Models\Region');
     }
 
     public function project()
@@ -61,39 +44,9 @@ class ExpenseReportLocation extends Model
         return $this->belongsTo('Ibinet\Models\Project');
     }
 
-    public function ticket()
-    {
-        return $this->belongsTo('Ibinet\Models\Ticket');
-    }
-
-    public function expense_report()
-    {
-        return $this->belongsTo('Ibinet\Models\ExpenseReport');
-    }
-
     public function expenseReport()
     {
         return $this->belongsTo('Ibinet\Models\ExpenseReport');
-    }
-
-    public function workType()
-    {
-        return $this->belongsTo('Ibinet\Models\WorkType')->withTrashed();
-    }
-
-    public function remote_helpdesk()
-    {
-        return $this->hasOne('Ibinet\Models\RemoteHelpdesk', 'expense_report_id');
-    }
-
-    public function remoteHelpdesk()
-    {
-        return $this->hasOne('Ibinet\Models\RemoteHelpdesk', 'expense_report_id');
-    }
-
-    public function remoteFinance()
-    {
-        return $this->hasOne('Ibinet\Models\RemoteFinance', 'expense_report_location_id');
     }
 
     public function adminProcessBy()
@@ -109,16 +62,6 @@ class ExpenseReportLocation extends Model
     public function financeProcessBy()
     {
         return $this->belongsTo('Ibinet\Models\User', 'finance_process_by');
-    }
-
-    public function remoteType()
-    {
-        return $this->belongsTo('Ibinet\Models\RemoteType');
-    }
-
-    public function region()
-    {
-        return $this->belongsTo('Ibinet\Models\Region');
     }
 
     /**
