dd37d8930ace6cb18c9035c368a2891ec80497ac
I Wayan Aditya Wiguna <adityawiguna0@gmail.com>
2025-02-15 10:20:23 +0800

    (new model) add model approval revision history

---
+++ Models/ApprovalRevisionHistory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ibinet\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
+
+class ApprovalRevisionHistory extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'approval_revision_histories';
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'created_at', 'updated_at'
+    ];
+
+    /**
+     *  Setup model event hooks
+     */
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->id = (string) Uuid::uuid4();
+        });
+    }
+}
