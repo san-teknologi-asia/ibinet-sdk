409f3dde04f7364527fa479319248070b9892b39
dededesnantha <dededesnantha@gmail.com>
2025-09-08 23:20:50 +0800

    fix role in cache

--- Helpers/PermissionHelper.php
+++ Helpers/PermissionHelper.php
@@ -8,7 +8,10 @@ function has($permission)
 {
     try {
         $authRoles = Auth::user()->role_id;
-        $roleHasPermission = Cache::get('permission');
+        $userId = Auth::user()->id;
+        $cacheKey = "permission_user_{$userId}_role_{$authRoles}";
+        
+        $roleHasPermission = Cache::get($cacheKey);
 
         if (!$roleHasPermission) {
             $roleHasPermission = RolePermission::where('role_id', $authRoles)
@@ -16,16 +19,10 @@ function has($permission)
                 ->pluck('permission_id')
                 ->toArray();
 
-            Cache::put('permission', $roleHasPermission, 1440);
+            Cache::put($cacheKey, $roleHasPermission, 1440);
         }
 
-        $roleHasPermission = in_array($permission, $roleHasPermission);
-
-        if ($roleHasPermission) {
-            return true;
-        }
-
-        return false;
+        return in_array($permission, $roleHasPermission);
     } catch (\Exception $e) {
         return false;
     }
